c595ad45d817378dea8b57bc2985378096939abf
Manuel Alejandro Muñoz Zeballos <manuel.munoz3@unmsm.edu.pe>
2024-04-15 16:23:06 -0500

    CambiosText
    Cambios en las descripciones

--- BotFinal.py
+++ BotFinal.py
@@ -20,13 +20,13 @@ def start_message(message):
 @bot.callback_query_handler(func=lambda call: call.data == "button1")
 def button1_callback(call):
     keyboard = types.InlineKeyboardMarkup()
-    reclamo1 = types.InlineKeyboardButton(text="Reclamo 1", callback_data="reclamo1")
-    reclamo2 = types.InlineKeyboardButton(text="Reclamo 2", callback_data="reclamo2")
-    reclamo3 = types.InlineKeyboardButton(text="Reclamo 3", callback_data="reclamo3")
-    reclamo4 = types.InlineKeyboardButton(text="Reclamo 4", callback_data="reclamo4")
+    reclamo1 = types.InlineKeyboardButton(text="Producto defectuoso", callback_data="reclamo1")
+    reclamo2 = types.InlineKeyboardButton(text="Problemas de entrega", callback_data="reclamo2")
+    reclamo3 = types.InlineKeyboardButton(text="Producto incorrecto", callback_data="reclamo3")
+    reclamo4 = types.InlineKeyboardButton(text="Problemas con el servicio al cliente", callback_data="reclamo4")
     back_button = types.InlineKeyboardButton(text="Retroceder", callback_data="back")
     keyboard.add(reclamo1, reclamo2, reclamo3, reclamo4, back_button)
-    bot.send_message(call.message.chat.id, "Estás en el flujo de Reclamos y quejas. \nElige una opción: \n1.Esta la opción del reclamo 1 \n2.Esta la opción del reclamo 2 \n3.Esta la opción del reclamo 3 \n4.Esta la opción del reclamo 4", reply_markup=keyboard)
+    bot.send_message(call.message.chat.id, "Estás en el flujo de Reclamos y quejas. \nElige una opción: \n1. Producto defectuoso \n2. Problemas de entrega \n3. Producto incorrecto \n4. Problemas con el servicio al cliente", reply_markup=keyboard)
 
 @bot.callback_query_handler(func=lambda call: call.data in ["reclamo1", "reclamo2", "reclamo3", "reclamo4"])
 def reclamo_callback(call):
@@ -34,13 +34,15 @@ def reclamo_callback(call):
     back_button = types.InlineKeyboardButton(text="Retroceder", callback_data="back")
     keyboard.add(back_button)
     if call.data == "reclamo1":
-        bot.send_message(call.message.chat.id, "Has seleccionado el Reclamo 1. Aquí está la solución para el Reclamo 1.", reply_markup=keyboard)
+        bot.send_message(call.message.chat.id, "Producto defectuoso", reply_markup=keyboard)
     elif call.data == "reclamo2":
-        bot.send_message(call.message.chat.id, "Has seleccionado el Reclamo 2. Aquí está la solución para el Reclamo 2.", reply_markup=keyboard)
+        bot.send_message(call.message.chat.id, "Problemas de entrega", reply_markup=keyboard)
     elif call.data == "reclamo3":
-        bot.send_message(call.message.chat.id, "Has seleccionado el Reclamo 3. Aquí está la solución para el Reclamo 3.", reply_markup=keyboard)
+        bot.send_message(call.message.chat.id, "Producto incorrecto", reply_markup=keyboard)
     elif call.data == "reclamo4":
-        bot.send_message(call.message.chat.id, "Has seleccionado el Reclamo 4. Aquí está la solución para el Reclamo 4.", reply_markup=keyboard)
+        bot.send_message(call.message.chat.id, "Problemas con el servicio al cliente", reply_markup=keyboard)
+
+
     
 
 @bot.callback_query_handler(func=lambda call: call.data == "button2")
@@ -54,12 +56,12 @@ def button2_callback(call):
 def button3_callback(call):
     keyboard = types.InlineKeyboardMarkup()
     back_button = types.InlineKeyboardButton(text="Retroceder", callback_data="back")
-    producto1 = types.InlineKeyboardButton(text="Producto 1", callback_data="producto1")
-    producto2 = types.InlineKeyboardButton(text="Producto 2", callback_data="producto2")
-    producto3 = types.InlineKeyboardButton(text="Producto 3", callback_data="producto3")
-    producto4 = types.InlineKeyboardButton(text="Producto 4", callback_data="producto4")
-    producto5 = types.InlineKeyboardButton(text="Producto 5", callback_data="producto5")
-    producto6 = types.InlineKeyboardButton(text="Producto 6", callback_data="producto6")
+    producto1 = types.InlineKeyboardButton(text="Electrodomesticos", callback_data="producto1")
+    producto2 = types.InlineKeyboardButton(text="Salud", callback_data="producto2")
+    producto3 = types.InlineKeyboardButton(text="Libros", callback_data="producto3")
+    producto4 = types.InlineKeyboardButton(text="Juguetes y juegos", callback_data="producto4")
+    producto5 = types.InlineKeyboardButton(text="Deportes y Fitness", callback_data="producto5")
+    producto6 = types.InlineKeyboardButton(text="Ropa", callback_data="producto6")
     keyboard.add(producto1)
     keyboard.add(producto2)
     keyboard.add(producto3)
@@ -67,7 +69,7 @@ def button3_callback(call):
     keyboard.add(producto5)
     keyboard.add(producto6)
     keyboard.add(back_button)
-    bot.send_message(call.message.chat.id, "Estás en el flujo de Nuestros productos. \nElige una opción: \n1.Esta la opción del Producto 1 \n2.Esta la opción del Producto 2 \n3.Esta la opción del Producto 3 \n4.Esta la opción del Producto 4 \n 5.Esta la opción del Producto 5 \n 6.Esta la opción del Producto 6 ", reply_markup=keyboard)
+    bot.send_message(call.message.chat.id, "Estás en el flujo de Nuestros productos. \nElige una opción: \n1.Electrodomesticos \n2. Salud\n3. Libros \n4. Juguetes y juegos\n 5. Deportes y Fitness \n 6. Ropa", reply_markup=keyboard)
 
 @bot.callback_query_handler(func=lambda call: call.data in ["producto1", "producto2", "producto3", "producto4", "producto5", "producto6"])
 def reclamo_callback(call):
@@ -75,22 +77,22 @@ def reclamo_callback(call):
     back_button = types.InlineKeyboardButton(text="Retroceder", callback_data="back")
     keyboard.add(back_button)
     if call.data == "producto1":
-        bot.send_message(call.message.chat.id, "Has seleccionado el Producto 1.")
+        bot.send_message(call.message.chat.id, "Has seleccionado Electrodomesticos")
         bot.send_document(call.message.chat.id, open('PDF/Productos.pdf', 'rb'))
     elif call.data == "producto2":
-        bot.send_message(call.message.chat.id, "Has seleccionado el Producto 2.")
+        bot.send_message(call.message.chat.id, "Has seleccionado Salud.")
         bot.send_document(call.message.chat.id, open('PDF/Productos.pdf', 'rb'))
     elif call.data == "producto3":
-        bot.send_message(call.message.chat.id, "Has seleccionado el Producto 3.")
+        bot.send_message(call.message.chat.id, "Has seleccionado Libros.")
         bot.send_document(call.message.chat.id, open('PDF/Productos.pdf', 'rb'))
     elif call.data == "producto4":
-        bot.send_message(call.message.chat.id, "Has seleccionado el Producto 4.")
+        bot.send_message(call.message.chat.id, "Has seleccionado Juguetes y juegos.")
         bot.send_document(call.message.chat.id, open('PDF/Productos.pdf', 'rb'))
     elif call.data == "producto5":
-        bot.send_message(call.message.chat.id, "Has seleccionado el Producto 5.")
+        bot.send_message(call.message.chat.id, "Has seleccionado Deportes y Fitness.")
         bot.send_document(call.message.chat.id, open('PDF/Productos.pdf', 'rb'))
     elif call.data == "producto6":
-        bot.send_message(call.message.chat.id, "Has seleccionado el Producto 6.")
+        bot.send_message(call.message.chat.id, "Has seleccionado Ropa.")
         bot.send_document(call.message.chat.id, open('PDF/Productos.pdf', 'rb'))
 
 
